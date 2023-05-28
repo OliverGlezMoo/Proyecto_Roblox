9cc92bf338a766e4b3bb428459a2dad4675fe9f8
OliverGlezMoo <123349304+OliverGlezMoo@users.noreply.github.com>
2023-05-28 04:13:29 -0600

    Delete Run.lua

--- Run.lua
+++
@@ -1,8 +0,0 @@
-game.ReplicatedStorage.EventRun.OnServerEvent:Connect(function(player, activador)
-	if activador == true then
-		player.Character.Humanoid.WalkSpeed = 50
-	else
-		player.Character.Humanoid.WalkSpeed = 16
-	end
-	
-end)
